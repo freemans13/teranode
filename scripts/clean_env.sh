7c22e0e78eabeced4ed327eea71174dc54c9b79c
teracoder <199657068+teracoder@users.noreply.github.com>
2023-09-07 00:02:46 +0100

    [Add] docker base image

---
+++ scripts/clean_env.sh
@@ -0,0 +1,29 @@
+# scale down everything
+kubectl scale deployment -n miner1 miner1 --replicas 0
+kubectl scale deployment -n miner2 miner2 --replicas 0
+kubectl scale deployment -n miner3 miner3 --replicas 0
+kubectl scale deployment -n tx-blaster-service tx-blaster --replicas 0
+kubectl scale deployment -n blob-service blob --replicas 0
+kubectl scale deployment -n coinbase-service coinbase --replicas 0
+kubectl scale deployment -n blockchain-service blockchain --replicas 0
+
+psql postgresql://coinbase:coinbase@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/coinbase -c "drop table coinbase_utxos; drop table state; drop table blocks;"
+psql postgresql://miner1:miner1@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/ubsv1 -c " drop table state; drop table utxos; drop table txmeta;drop table blocks;"
+psql postgresql://miner2:miner2@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/ubsv2 -c " drop table state; drop table utxos; drop table txmeta;drop table blocks;"
+psql postgresql://miner3:miner3@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/ubsv3 -c " drop table state; drop table utxos; drop table txmeta;drop table blocks;"
+psql postgresql://m1:m1@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/m1 -c " drop table if exists state ; drop table if exists blocks;"
+
+# tood make key dynamic based on user
+ssh -i ~/.ssh/joe-ssh-aws-ubsv.pem  ubuntu@aero.utxo0.ubsv.dev
+aql "TRUNCATE utxostore" # todo fix how to run this dynamically
+
+# scale back up everything
+kubectl scale deployment -n coinbase-service coinbase --replicas 1
+kubectl scale deployment -n blockchain-service blockchain --replicas 1
+kubectl scale deployment -n miner1 miner1 --replicas 1
+kubectl scale deployment -n miner2 miner2 --replicas 1
+kubectl scale deployment -n miner3 miner3 --replicas 1
+kubectl scale deployment -n blob-service blob --replicas 1
+
+
+kubectl scale deployment -n tx-blaster-service tx-blaster --replicas 1
