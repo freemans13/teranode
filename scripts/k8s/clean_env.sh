736253cdadaaade78f8fc2f0d00d2cb7994d21ff
teracoder <199657068+teracoder@users.noreply.github.com>
2023-09-15 19:13:31 +0100

    [Fix] resource and time out build on rollout

--- scripts/k8s/clean_env.sh
+++ scripts/k8s/clean_env.sh
@@ -1,12 +1,13 @@
 # scale down everything
 bash scale_down.sh
 
-psql postgresql://coinbase:coinbase@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/coinbase -c "drop table coinbase_utxos; drop table state; drop table blocks;"
+
 psql postgresql://coinbase_allinone:coinbase_allinone@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/coinbase_allinone -c "drop table coinbase_utxos; drop table state; drop table blocks;"
 psql postgresql://miner1:miner1@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/ubsv1 -c " drop table state; drop table utxos; drop table txmeta;drop table blocks;"
 psql postgresql://miner2:miner2@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/ubsv2 -c " drop table state; drop table utxos; drop table txmeta;drop table blocks;"
 psql postgresql://miner3:miner3@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/ubsv3 -c " drop table state; drop table utxos; drop table txmeta;drop table blocks;"
 psql postgresql://m1:m1@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/m1 -c " drop table if exists state ; drop table if exists blocks;"
+psql postgresql://coinbase:coinbase@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/coinbase -c "drop table coinbase_utxos; drop table state; drop table blocks;"
 
 # todo make key dynamic based on user
 ssh -i ~/.ssh/joe-ssh-aws-ubsv.pem  ubuntu@aero.utxo0.ubsv.dev -f "aql -c \"TRUNCATE utxostore\""
