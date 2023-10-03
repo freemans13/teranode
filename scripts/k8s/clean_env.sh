03784fd76976fc33d6e1f4dab32fc824eafcc507
teracoder <199657068+teracoder@users.noreply.github.com>
2023-10-03 22:49:15 +0100

    [Fix] clean env.sh

--- scripts/k8s/clean_env.sh
+++ scripts/k8s/clean_env.sh
@@ -10,12 +10,12 @@ psql postgresql://miner3:miner3@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaw
 psql postgresql://m1:m1@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/m1 -c " drop table if exists state ; drop table if exists blocks;"
 psql postgresql://coinbase:coinbase@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/coinbase -c "drop table coinbase_utxos; drop table state; drop table blocks;"
 # truncate
-$ psql postgresql://m1:m1@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/m1 -c "truncate table state CASCADE; truncate table blocks CASCADE; truncate table coinbase_utxos CASCADE;"
-$ psql postgresql://coinbase:coinbase@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/coinbase -c "truncate table state CASCADE; truncate table blocks CASCADE; truncate table coinbase_utxos CASCADE;"
+psql postgresql://m1:m1@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/m1 -c "truncate table state CASCADE; truncate table blocks CASCADE; truncate table coinbase_utxos CASCADE;"
+psql postgresql://coinbase:coinbase@services-db.cipebxcxpkpk.eu-north-1.rds.amazonaws.com:5432/coinbase -c "truncate table state CASCADE; truncate table blocks CASCADE; truncate table coinbase_utxos CASCADE;"
 
 # todo make key dynamic based on user
-ssh -i ~/.ssh/joe-ssh-aws-ubsv.pem  ubuntu@aero.utxo0.ubsv.dev -f "aql -c \"TRUNCATE utxostore\""
-ssh -i ~/.ssh/joe-ssh-aws-ubsv.pem  ubuntu@aero.tx0.ubsv.dev -f "aql -c \"TRUNCATE txstatus_store\""
+ssh -i ~/.ssh/joe-ssh-aws-ubsv.pem ubuntu@aero.utxo-store3.eu-north-1.ubsv.dev -f "aql -c \"TRUNCATE utxostore\""
+ssh -i ~/.ssh/joe-ssh-aws-ubsv.pem ubuntu@aero.tx-status-store4.eu-north-1.ubsv.dev -f "aql -c \"TRUNCATE txstatus_store\""
 
 # scale back up everything
 bash scale_up.sh
