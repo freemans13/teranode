d242fd970010c5e1881a1dec798adf1540c0410f
freemans13 <freemans13@users.noreply.github.com>
2023-10-12 09:33:41 +0100

    wait for max 30 seconds

--- scripts/k8s/scale_up.sh
+++ scripts/k8s/scale_up.sh
@@ -8,27 +8,27 @@ wait() {
 
 # order is important here, do not change unless you know what you're doing
 kubectl scale deployment -n m1 blockchain1 --replicas 1
-wait m1 blockchain1 10
+wait m1 blockchain1 30
 kubectl scale deployment -n m1 utxostore1 --replicas 1
-wait m1 utxostore1 10
+wait m1 utxostore1 30
 kubectl scale deployment -n m1 txmetastore1 --replicas 1
-wait m1 txmetastore1 10
+wait m1 txmetastore1 30
 kubectl scale deployment -n m1 blockassembly1 --replicas 1
-wait m1 blockassembly1 10
+wait m1 blockassembly1 30
 kubectl scale deployment -n m1 blockvalidation1 --replicas 1
-wait m1 blockvalidation1 10
+wait m1 blockvalidation1 30
 kubectl scale deployment -n m1 blob1 --replicas 1
-wait m1 blob1 10
+wait m1 blob1 30
 kubectl scale deployment -n m1 coinbase1 --replicas 1
-wait m1 coinbase1 10
+wait m1 coinbase1 30
 kubectl scale deployment -n m1 validation1 --replicas 1
-wait m1 validation1 10
+wait m1 validation1 30
 kubectl scale deployment -n m1 propagation1 --replicas 1
-wait m1 propagation1 10
+wait m1 propagation1 30
 kubectl scale deployment -n m1 miner1 --replicas 1
 
 
 # all in one
-kubectl scale deployment -n miner1 miner1 --replicas 1
-kubectl scale deployment -n miner2 miner2 --replicas 1
-kubectl scale deployment -n miner3 miner3 --replicas 1
+# kubectl scale deployment -n miner1 miner1 --replicas 1
+# kubectl scale deployment -n miner2 miner2 --replicas 1
+# kubectl scale deployment -n miner3 miner3 --replicas 1
