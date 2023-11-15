92874fbd4c01b22aac70690f5651f6c056cd3b74
freemans13 <freemans13@users.noreply.github.com>
2023-11-15 05:37:12 +0000

    tweak order of scale up and scale down

--- scripts/k8s/scale_up.sh
+++ scripts/k8s/scale_up.sh
@@ -19,12 +19,12 @@ kubectl scale deployment -n m1 blockassembly1 --replicas 1
 wait m1 blockassembly1 30
 kubectl scale deployment -n m1 blockvalidation1 --replicas 1
 wait m1 blockvalidation1 30
-kubectl scale deployment -n m1 coinbase1 --replicas 1
-wait m1 coinbase1 30
 kubectl scale deployment -n m1 validation1 --replicas 1
 wait m1 validation1 30
 kubectl scale deployment -n m1 propagation1 --replicas 1
 wait m1 propagation1 30
+kubectl scale deployment -n m1 coinbase1 --replicas 1
+wait m1 coinbase1 30
 kubectl scale deployment -n m1 miner1 --replicas 1
 
 
