c62bae78c02299435c7ade3f921a13d277fe5cb6
freemans13 <freemans13@users.noreply.github.com>
2023-11-15 01:50:18 +0000

    start blob server sooner

--- scripts/k8s/scale_up.sh
+++ scripts/k8s/scale_up.sh
@@ -13,12 +13,12 @@ wait m1 blockchain1 30
 #wait m1 utxostore1 30
 #kubectl scale deployment -n m1 txmetastore1 --replicas 1
 #wait m1 txmetastore1 30
+kubectl scale deployment -n m1 blob1 --replicas 1
+wait m1 blob1 30
 kubectl scale deployment -n m1 blockassembly1 --replicas 1
 wait m1 blockassembly1 30
 kubectl scale deployment -n m1 blockvalidation1 --replicas 1
 wait m1 blockvalidation1 30
-kubectl scale deployment -n m1 blob1 --replicas 1
-wait m1 blob1 30
 kubectl scale deployment -n m1 coinbase1 --replicas 1
 wait m1 coinbase1 30
 kubectl scale deployment -n m1 validation1 --replicas 1
