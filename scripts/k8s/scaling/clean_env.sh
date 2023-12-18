bee28299756483c4566722fbd5a2220d7aaaf95e
Simon Ordish <simon.ordish@masagi.co.uk>
2023-12-18 14:04:17 +0100

    Clean up local Postgres databases in different regions

--- scripts/k8s/scaling/clean_env.sh
+++ scripts/k8s/scaling/clean_env.sh
@@ -18,9 +18,28 @@ psql postgres://m3:m3@miners-db.cfhjsqgwu9jw.ap-northeast-1.rds.amazonaws.com:54
 psql postgres://coinbase:coinbase@miners-db.cfhjsqgwu9jw.ap-northeast-1.rds.amazonaws.com:5432/coinbase -c "drop table if exists spendable_utxos; drop table if exists coinbase_utxos; drop table if exists state; drop table if exists  blocks;"
 # endregion
 
+CONTEXT=$(kubectl config current-context)
+
+# Local databases
+echo "Postgres cleaning: EU"
+kubectl config use-context arn:aws:eks:eu-west-1:434394763103:cluster/aws-ubsv-playground
+kubectl exec -it -n postgres postgres-postgresql-0 -- env PGPASSWORD=m1 psql -U m1 -d m1 -c "drop table if exists state ; drop table if exists blocks;"
+kubectl exec -it -n postgres postgres-postgresql-0 -- env PGPASSWORD=coinbase psql -U coinbase -d coinbase -c "drop table if exists state ; drop table if exists blocks;"
+
+
+echo "Postgres cleaning: US"
+kubectl config use-context arn:aws:eks:us-east-1:434394763103:cluster/aws-ubsv-playground
+kubectl exec -it -n postgres postgres-postgresql-0 -- env PGPASSWORD=m2 psql -U m2 -d m2 -c "drop table if exists state ; drop table if exists blocks;"
+kubectl exec -it -n postgres postgres-postgresql-0 -- env PGPASSWORD=coinbase psql -U coinbase -d coinbase -c "drop table if exists state ; drop table if exists blocks;"
+
+# echo "Postgres cleaning: Asia"
+# kubectl config use-context arn:aws:eks:eu-west-1:434394763103:cluster/aws-ubsv-playground
+# kubectl exec -it -n postgres postgres-postgresql-0 -- env PGPASSWORD=m3 psql -U m3 -d m3 -c "drop table if exists state ; drop table if exists blocks;"
+# kubectl exec -it -n postgres postgres-postgresql-0 -- env PGPASSWORD=coinbase psql -U coinbase -d coinbase -c "drop table if exists state ; drop table if exists blocks;"
+
+
 # todo make key dynamic based on user
 echo "Aerospike cleaning"
-CONTEXT=$(kubectl config current-context)
 
 echo "Aerospike cleaning: EU"
 kubectl config use-context arn:aws:eks:eu-west-1:434394763103:cluster/aws-ubsv-playground
