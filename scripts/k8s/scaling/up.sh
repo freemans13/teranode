440b8bc0354642159f634407f7816262e2e6c7a0
Simon Ordish <simon.ordish@masagi.co.uk>
2024-02-22 14:21:30 +0000

    Clean environments in parallel

--- scripts/k8s/scaling/up.sh
+++ scripts/k8s/scaling/up.sh
@@ -3,7 +3,6 @@ if [ -n "$KUBECONFIG" ]; then
   exit 1
 fi
 
-CONTEXT=$(kubectl config current-context)
 if [ "$1" == "help" ]; then
   echo "Usage: $0 [all|eu|m1|us|m2|asia|m3]"
   echo "all: scale up all regions"
@@ -13,77 +12,81 @@ if [ "$1" == "help" ]; then
   exit 0
 fi
 
-wait() {
-  local namespace=$1
-  local deployment_name=$2
-  local timeout=$3
+wait_for_scale() {
+  local region=$1
+  local namespace=$2
+  local deployment_name=$3
+  local timeout=$4
+
   echo "Waiting for healthy deployment $deployment_name in namespace $namespace"
-  kubectl -n "$namespace" wait --for=condition=available --timeout="${timeout}s" deployment/"$deployment_name"
+  kubectl -n "$namespace" --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground wait --for=condition=available --timeout="${timeout}s" deployment/"$deployment_name"
 }
 
 # order is important here, do not change unless you know what you're doing
 scale_up() {
-  kubectl scale deployment -n m$namespace_suffix blockchain$namespace_suffix --replicas 1
-  wait m$namespace_suffix blockchain$namespace_suffix 30
-  # kubectl scale deployment -n m$namespace_suffix status$namespace_suffix --replicas 1
-  # wait m$namespace_suffix status$namespace_suffix 30
+  local region=$1
+  local namespace_suffix=$2
+  
+  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground blockchain$namespace_suffix --replicas 1
+  wait_for_scale $region m$namespace_suffix blockchain$namespace_suffix 30
+  # kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground status$namespace_suffix --replicas 1
+  # wait_for_scale $region m$namespace_suffix status$namespace_suffix 30
   # asset/blockvalidation/blockassembly need to be scaled up together as they depend on each other :(
-  kubectl scale deployment -n m$namespace_suffix asset$namespace_suffix --replicas 1
-  kubectl scale deployment -n m$namespace_suffix blockvalidation$namespace_suffix --replicas 1
-  kubectl scale deployment -n m$namespace_suffix blockassembly$namespace_suffix --replicas 1
-  kubectl scale deployment -n m$namespace_suffix faucet$namespace_suffix --replicas 1
-  kubectl scale deployment -n m$namespace_suffix nginx-reverse-proxy --replicas 1
+  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground asset$namespace_suffix --replicas 1
+  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground blockvalidation$namespace_suffix --replicas 1
+  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground blockassembly$namespace_suffix --replicas 1
+  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground faucet$namespace_suffix --replicas 1
+  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground nginx-reverse-proxy --replicas 1
   # wait for all 3 to be ready
-  wait m$namespace_suffix asset$namespace_suffix 30
-  wait m$namespace_suffix blockvalidation$namespace_suffix 30
-  wait m$namespace_suffix blockassembly$namespace_suffix 30
-  kubectl scale deployment -n m$namespace_suffix propagation$namespace_suffix --replicas 1
-  wait m$namespace_suffix propagation$namespace_suffix 30
-  kubectl scale deployment -n m$namespace_suffix p2p$namespace_suffix --replicas 1
-  wait m$namespace_suffix p2p$namespace_suffix 30
+  wait_for_scale $region m$namespace_suffix asset$namespace_suffix 30
+  wait_for_scale $region m$namespace_suffix blockvalidation$namespace_suffix 30
+  wait_for_scale $region m$namespace_suffix blockassembly$namespace_suffix 30
+  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground propagation$namespace_suffix --replicas 1
+  wait_for_scale $region m$namespace_suffix propagation$namespace_suffix 30
+  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground p2p$namespace_suffix --replicas 1
+  wait_for_scale $region m$namespace_suffix p2p$namespace_suffix 30
   echo "Not scaling coinbase and miner as you need to be careful of order when booting the blockchain"
   # todo think of better way to bring up the nodes
-#  kubectl scale deployment -n m$namespace_suffix coinbase$namespace_suffix --replicas 1
-#  wait m$namespace_suffix coinbase$namespace_suffix 30
-#  kubectl scale deployment -n m$namespace_suffix miner$namespace_suffix --replicas 1
+#  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground coinbase$namespace_suffix --replicas 1
+#  wait_for_scale $region m$namespace_suffix coinbase$namespace_suffix 30
+#  kubectl scale deployment -n m$namespace_suffix --context arn:aws:eks:$region:434394763103:cluster/aws-ubsv-playground miner$namespace_suffix --replicas 1
 }
 
+# Array to hold PIDs of background processes
+bg_pids=()
+
 if [ "$1" == "all" ]; then
-  kubectl config use-context arn:aws:eks:eu-west-1:434394763103:cluster/aws-ubsv-playground
-  namespace_suffix=1
-  scale_up
+  scale_up "eu-west-1" "1" &
+  bg_pids+=($!)
+  scale_up "us-east-1" "2" &
+  bg_pids+=($!)
+  scale_up "ap-south-1" "3" &
+  bg_pids+=($!)
 
-  kubectl config use-context arn:aws:eks:us-east-1:434394763103:cluster/aws-ubsv-playground
-  namespace_suffix=2
-  scale_up
-
-  kubectl config use-context arn:aws:eks:ap-south-1:434394763103:cluster/aws-ubsv-playground
-  namespace_suffix=3
-  scale_up
 else
   if [[ "$1" == "eu" || "$1" == "m1" ]]; then
-    kubectl config use-context arn:aws:eks:eu-west-1:434394763103:cluster/aws-ubsv-playground
-    namespace_suffix=1
+    scale_up "eu-west-1" "1"
   elif [[ "$1" == "us" || "$1" == "m2" ]]; then
-    kubectl config use-context arn:aws:eks:us-east-1:434394763103:cluster/aws-ubsv-playground
-    namespace_suffix=2
+    scale_up "us-east-1" "2"
   elif [[ "$1" == "asia" || "$1" == "m3" ]]; then
-    kubectl config use-context arn:aws:eks:ap-south-1:434394763103:cluster/aws-ubsv-playground
-    namespace_suffix=3
+    scale_up "ap-south-1" "3"
   else
     echo "Auto detecting environment"
     if [[ $(kubectl config current-context) == *"eu-west-1"* ]]; then
-      namespace_suffix=1
+      scale_up "eu-west-1" "1"
     elif [[ $(kubectl config current-context) == *"us-east-1"* ]]; then
-      namespace_suffix=2
+      scale_up "us-east-1" "2"
     elif [[ $(kubectl config current-context) == *"ap-south-1"* ]]; then
-      namespace_suffix=3
+      scale_up "ap-south-1" "3"
     else
       echo "Unknown context, cannot scale down. Change namespace and try again"
       exit 1
     fi
   fi
-  scale_up
 fi
 
-kubectl config use-context $CONTEXT
+# Wait for all background processes to complete
+for pid in "${bg_pids[@]}"; do
+  wait "$pid" || echo "Process $pid exited with status $?"
+done
+
