ec7069b21d5d3dacf77e8b9c9820e7d64544d9e2
freemans13 <stu@team-freeman.com>
2026-05-21 17:38:53 +0100

    Revert "perf(legacy/netsync): also copy tx hash in createTxMap to drop wire.MsgTx pinning"
    This reverts commit 9132681b7b134c0c1fd8eab90bc3d04dce5b4dc6.

--- services/legacy/netsync/handle_block.go
+++ services/legacy/netsync/handle_block.go
@@ -1311,13 +1311,8 @@ func (sm *SyncManager) createTxMap(ctx context.Context, block *bsvutil.Block, tx
 
 		// don't add the coinbase to the txMap, we cannot process it anyway
 		if !tx.IsCoinbase() {
-			// Copy the hash value out of the bsvutil.Tx wrapper. bt.Tx.SetTxHash
-			// stores the pointer, so passing wireTx.Hash() directly would keep
-			// the wrapping wire.MsgTx (and its decode arena) alive through this
-			// bt.Tx and the TxMapWrapper it lands in.
-			hashCopy := *wireTx.Hash()
-			tx.SetTxHash(&hashCopy)
-			txMap.Set(hashCopy, &TxMapWrapper{Tx: tx})
+			tx.SetTxHash(wireTx.Hash())
+			txMap.Set(*tx.TxIDChainHash(), &TxMapWrapper{Tx: tx})
 		}
 	}
 
